2738262ab3610a77bcb4ed04aa761cf6bc02243c
MrSolimanKing <46472036+MrSolimanKing@users.noreply.github.com>
2022-01-01 06:54:38 -0800

    Add files via upload

---
+++ Backend/ecommerc/ecommerc/urls.py
@@ -0,0 +1,36 @@
+"""ecommerc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from user.models import Account
+from product.models import Product
+
+urlpatterns = [
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
+
+
+    path('admin/', admin.site.urls),
+    path("user/", include("user.urls", namespace="user")),
+    path("product/", include("product.urls", namespace="product"))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
